9d368062cd3cb2141824ff3a1cce2306168912c0
Lane <lanedaltonjohns@gmail.com>
2021-01-21 18:59:08 -0500

    create logout API route

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -30,4 +30,13 @@ router.post(
     }),
 );
 
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+        res.clearCookie('token');
+        return res.json({ message: 'success' })
+    }
+);
+
 module.exports = router;
