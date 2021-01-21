bb33f3c96126a1f686d2ad1eb1db758d6f902b10
Lane <lanedaltonjohns@gmail.com>
2021-01-21 18:52:46 -0500

    add basic user auth

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,8 +1,49 @@
 const router = require('express').Router();
+const sessionRouter = require('./session')
+const usersRouter = require('./users')
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body })
-})
+router.use('/session', sessionRouter)
+
+router.use('/users', usersRouter)
+
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+
+
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-User'
+//       },
+//     })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+
+// const { restoreUser } = require('../../utils/auth.js');
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body })
+// })
 
 
 module.exports = router;
