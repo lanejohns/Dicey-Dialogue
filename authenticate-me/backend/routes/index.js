107b6297257588f47d4044934be0dfda8c15539f
Lane <lanedaltonjohns@gmail.com>
2021-01-22 12:46:49 -0500

    create react/redux skeleton for frontend

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -4,9 +4,9 @@ const apiRouter = require("./api")
 
 router.use('/api', apiRouter)
 
-router.get("/hello/world", function(req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken())
-    res.send('Hello World!')
-})
+// router.get("/hello/world", function(req, res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken())
+//     res.send('Hello World!')
+// })
 
 module.exports = router;
