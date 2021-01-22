20042ab5f0a9aa8a907d99bae0a3e3f3a10ae2b3
Lane <lanedaltonjohns@gmail.com>
2021-01-22 08:55:46 -0500

    create login validation

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler')
+const { check } = require('express-validator')
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth')
 const { User } = require('../../db/models')
+const { handleValidationErrors } = require('../../utils/validation')
 
 const router = express.Router();
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors,
+];
 
 // Log in
 router.post(
     '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
         const { credential, password } = req.body;
 
