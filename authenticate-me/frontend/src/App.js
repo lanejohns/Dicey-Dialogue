f4fbcd42dedd1d84584ffe001c6545330361efc9
Lane <lanedaltonjohns@gmail.com>
2021-01-22 13:47:44 -0500

    create login component

--- authenticate-me/frontend/src/App.js
+++ authenticate-me/frontend/src/App.js
@@ -1,6 +1,13 @@
+import { Route, Switch } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage/LoginFormPage'
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
