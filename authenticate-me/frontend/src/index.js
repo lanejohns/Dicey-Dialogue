fbe71f4188ed7d414114298c6aac0f7e559ed236
Lane <lanedaltonjohns@gmail.com>
2021-01-22 13:05:26 -0500

    added csrf to frontend

--- authenticate-me/frontend/src/index.js
+++ authenticate-me/frontend/src/index.js
@@ -5,10 +5,15 @@ import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import { restoreCSRF, fetch } from './store/csrf';
+
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = fetch;
   window.store = store;
 }
 
