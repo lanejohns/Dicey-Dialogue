2973e1ad433ab42680b1f21f0bc4256458de89f5
Lane <lanedaltonjohns@gmail.com>
2021-01-24 12:57:36 -0500

    make it better

--- authenticate-me/backend/config/database.js
+++ authenticate-me/backend/config/database.js
@@ -19,6 +19,13 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
+    dialectOptions: {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
+},
+
   },
 };
 
